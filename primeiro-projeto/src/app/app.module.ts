3beef45ff583b78cbe651d45fb74f88ffea8a722
Alécio <alecio.godoi@ymail.com>
2020-09-10 11:43:54 -0300

    Aula 04 10-09-2020 - Exemplos Binding -  Diretivas *ngIf e *ngFor

--- primeiro-projeto/src/app/app.module.ts
+++ primeiro-projeto/src/app/app.module.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
@@ -31,34 +30,3 @@ import { DiretivaNgforComponent } from './diretiva-ngfor/diretiva-ngfor.componen
   bootstrap: [AppComponent]
 })
 export class AppModule { }
-||||||| merged common ancestors
-=======
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { PrimeiroComponent } from './primeiro/primeiro.component';
-import { SegundoComponent } from './segundo/segundo.component';
-import { TerceiroComponent } from './terceiro/terceiro.component';
-import { FormsModule } from '@angular/forms';
-import { ExemplosBindingComponent } from './exemplos-binding/exemplos-binding.component';​
-
-@NgModule({
-  declarations: [
-    AppComponent,
-    PrimeiroComponent,
-    SegundoComponent,
-    TerceiroComponent,
-    ExemplosBindingComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    FormsModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
-})
-export class AppModule { }
->>>>>>> cfad5bb9dbb82f5a07c7f10d37cc13b59dca1293
