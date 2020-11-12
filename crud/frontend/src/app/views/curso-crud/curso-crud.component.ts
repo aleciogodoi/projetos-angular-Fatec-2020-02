9f0edf9d1848862adf9299f9c9f5d32f0edae3ed
Alécio <alecio.godoi@ymail.com>
2020-11-12 10:55:05 -0300

    Aula 13 - 12-11-2020 - 02

--- crud/frontend/src/app/views/curso-crud/curso-crud.component.ts
+++ crud/frontend/src/app/views/curso-crud/curso-crud.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-curso-crud',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CursoCrudComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
+  }
+
+  navigateToCursoCreate() {
+    this.router.navigate(['/curso/create']);
   }
 
 }
